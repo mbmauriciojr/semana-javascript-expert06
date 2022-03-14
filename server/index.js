601dab1d3b2aa0fa712a6fdd8e83b7f0acb26d76
Marcelo Maurício <mmauricio.design@gmail.com>
2022-03-14 15:44:06 -0300

    Imported the config to using in the index server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
+import config from './config/config.js';
 import server from './server.js';
 import { logger } from './util.js';
 
-server.listen(3000)
-  .on('listening', () => logger.info('server running!'))
+server.listen(config.port)
+  .on('listening', () => logger.info(`Server running at ${config.port}!!`));

a049bf105f28c41e7a2339389ceb2727c609099b
Marcelo Maurício <mmauricio.design@gmail.com>
2022-03-14 16:24:58 -0300

    Created the routes and the responses for them

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,39 @@
-export function handler(request, response) {
+import config from "../config/config.js";
+import { Controller } from "../controller/controller.js";
+import { logger } from '../util.js';
+
+const { 
+  location,
+  pages: {
+    homeHTML,
+  },
+} = config;
+
+async function routes(request, response) {
+  const { method, url } = request;
+
+const controller = new Controller()
+
+  if(method === 'GET' && url === '/') {
+    response.writeHead(302, {
+      'Location': location.home,
+    });
+
+    return response.end();
+  };
+
+  if(method === 'GET' && url === '/home') {
+    const {
+      stream,
+    } = await controller.getFileStream(homeHTML);
+
+    return stream.pipe(response);
+  };
+
   return response.end('Hello');
+};
+
+export function handler(request, response) {
+  return routes(request, response)
+    .catch(error => logger.error(`Algo deu eerrado: ${error.stack}`));
 };
